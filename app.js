cacfb95915ee325c7ccdcab9a14bef908b19c501
geovanna <vannasousa28@gmail.com>
2025-10-30 22:42:55 -0300

    Alteração no espaçamento

--- app.js
+++ app.js
@@ -2,28 +2,66 @@
 
 const cards = document.querySelectorAll('.habilidades');
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            }
-        });
-    }, { threshold: 0.2 });
-
-    cards.forEach(card => {
-        observer.observe(card);
-    });
-
-        const text = "Desenvolvedora Júnior com foco em Backend.";
-    const typingElement = document.getElementById("typing-text");
-    let index = 0;
-
-    function typeEffect() {
-        if (index < text.length) {
-            typingElement.innerHTML += text.charAt(index);
-            index++;
-            setTimeout(typeEffect, 60); // velocidade da digitação
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
         }
-    }
+    });
+}, { threshold: 0.5 });
 
-    typeEffect();
+cards.forEach(card => {
+    observer.observe(card);
+});
+
+
+// Animação de digitação
+const text = "Desenvolvedora Júnior com foco em Backend.";
+const typingElement = document.getElementById("typing-text");
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        typingElement.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 60); // velocidade da digitação
+    }
+}
+
+typeEffect();
+
+
+const footer = document.querySelector('footer');
+
+function checkFooterVisibility() {
+    const rect = footer.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    if (rect.top < windowHeight && rect.bottom > 0) {
+        footer.classList.add('show-footer');
+    } else {
+        footer.classList.remove('show-footer');
+    }
+}
+
+window.addEventListener('scroll', checkFooterVisibility);
+window.addEventListener('load', checkFooterVisibility);
+
+
+
+
+const workSection = document.querySelector('.work');
+
+function checkWorkVisibility() {
+    const rect = workSection.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    if (rect.top < windowHeight && rect.bottom > 0) {
+        workSection.classList.add('show-work');
+    } else {
+        workSection.classList.remove('show-work');
+    }
+}
+
+window.addEventListener('scroll', checkWorkVisibility);
+window.addEventListener('load', checkWorkVisibility);
